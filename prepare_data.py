c42e6a4f6139630188925ea5d2da49fbcf2d87ae
PaulJeha <paul.jeha@gmail.com>
2023-05-06 23:35:01 +0200

    tmp fix for cataegorical data

--- prepare_data.py
+++ prepare_data.py
@@ -46,12 +46,17 @@ def augment_dataset_with_mask(
     args_dict,
     complete_dataset,
 ):
-    complete_masked_dataset = create_mask_dataset(
-        args_dict,
-        complete_dataset,
-        DATASETS_TENSOR=DATASETS_TENSOR,
-        dic_image_dataset=dic_image_dataset,
-    )
+    if args_dict["dataset_name"] not in dic_discrete_dataset:
+        complete_masked_dataset = create_mask_dataset(
+            args_dict,
+            complete_dataset,
+            DATASETS_TENSOR=DATASETS_TENSOR,
+            dic_image_dataset=dic_image_dataset,
+        )
+    else:
+        complete_masked_dataset = get_discrete_dataset(
+            args_dict,
+        )
     return complete_masked_dataset
 
 
